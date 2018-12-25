d0fbe19e28533ded40e0761e1e8b18559db9b9f3
raddevus <1radeutsch1@gmail.com>
2018-12-25 11:45:14 -0500

    Initial creation of project.  Added picker view for bluetooth device list.  Added event so when user selects item I can save btdevice name to nsuserdefaults.

--- DoorMaster/ViewController.swift
+++ DoorMaster/ViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return BTDevices.count
+    }
+    
+    let userDefs = UserDefaults.standard
+    var BTDevices : [String] = [String]()
+    
+    @IBOutlet weak var BTPicker: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Connect data:
+        self.BTPicker.delegate = self
+        self.BTPicker.dataSource = self
+        
+        BTDevices = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return BTDevices[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //userDefs.string(forKey: "btDevice")
+        var x = 5
     }
 
 
